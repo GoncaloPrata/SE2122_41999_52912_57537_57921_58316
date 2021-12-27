3a62eef321244601bb135068f744d1948b8bc0e7
marianabmaximiano <m.maximiano@campus.fct.unl.pt>
2021-12-27 11:24:44 +0000

    implementação e commit da classe correspondente à primeira funcionalidade

--- src/main/java/org/jabref/serpapiAPI/FirstFunctionality.java
+++ src/main/java/org/jabref/serpapiAPI/FirstFunctionality.java
@@ -1,5 +1,6 @@
 package org.jabref.serpapiAPI;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 import java.util.List;
@@ -12,7 +13,10 @@ public class FirstFunctionality {
         this.jsonObjectWithProfileData = object;
     }
 
-    // TODO
+    /**
+     * The method generates the output of all methods
+     * @return the output of all methods
+     */
     public List<String> generateOutput() {
         List<String> output = generateGeneralInformation();
         output.addAll(generateArticlesWrittenByAuthor());
@@ -20,21 +24,64 @@ public class FirstFunctionality {
         return output;
     }
 
-    // TODO
+    /**
+     * The method searches the general information about an author
+     * @return the general information of the author
+     */
     public List<String> generateGeneralInformation() {
         List<String> output = null;
+        JsonObject author = jsonObjectWithProfileData.get("author").getAsJsonObject(); // Subsecção do JSON "author"
+        String nameOfAuthor = author.get("name").getAsString();
+        String affiliationsOfAuthor = author.get("affiliations").getAsString();
+        output.add("Nome: " + nameOfAuthor + "\nAfiliações: " + affiliationsOfAuthor + "\nInteresses: ");
+        JsonArray interestsAuthor = author.get("interests").getAsJsonArray();
+        for (JsonElement interesses : interestsAuthor) {
+            output.add("    " + interesses.getAsJsonObject().get("title").getAsString());
+        }
+
         return output;
+
     }
 
-    // TODO
+    /**
+     * The method lists the articles by an author, including the article's title, link and authors
+     * @return the articles by an author
+     */
     public List<String> generateArticlesWrittenByAuthor() {
         List<String> output = null;
+        output.add("Artigos:");
+        JsonArray artigosAutor = jsonObjectWithProfileData.getAsJsonArray("articles");
+        for (JsonElement artigos : artigosAutor) {
+            String titleArticle = artigos.getAsJsonObject().get("title").getAsString();
+            String linkArticle = artigos.getAsJsonObject().get("link").getAsString();
+            String authorsArticle = artigos.getAsJsonObject().get("authors").getAsString();
+            output.add("    Title: " + titleArticle +
+                    "\n    Link: " + linkArticle +
+                    "\n    Autores: " + authorsArticle + "\n\n");
+        }
         return output;
     }
 
-    //TODO
+    /**
+     * The method lists the coAuthors
+     * @return the coAuthors
+     */
     public List<String> generateCoAuthors() {
         List<String> output = null;
+        output.add("Co-autores:");
+        JsonArray coauthors = jsonObjectWithProfileData.getAsJsonArray("co_authors");
+        for (JsonElement co_author : coauthors) {
+            String nameCo_Author = co_author.getAsJsonObject().get("name").getAsString();
+            String linkCo_Author = co_author.getAsJsonObject().get("link").getAsString();
+            String affiliationsCo_Author = "No perfil Google Scholar não estão mencionadas afiliações.";
+            if (co_author.getAsJsonObject().has("affiliations"))
+                affiliationsCo_Author = co_author.getAsJsonObject().get("affiliations").getAsString();
+            output.add("    Name of Coauthor: " + nameCo_Author +
+                    "\n    Link: " + linkCo_Author +
+                    "\n    Affiliations: " + affiliationsCo_Author + "\n\n");
+        }
         return output;
     }
+
+
 }
